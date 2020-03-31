25cb1a2ab913b6849f0599ec1a22522f643196f6
Denis Barreto <barretohanks@gmail.com>
2020-03-30 19:55:41 -0500

    formulario

--- JavaScript/codigo.js
+++ JavaScript/codigo.js
@@ -1,12 +1,26 @@
-    function clickk() {
+    Swal.fire({
+        title:'Bienvenidos a mi sitio Web',
+        text:'espero que te guste',
+        icon: 'info'
+    });
+    function clickk(event) {
         var input = document.getElementById('input');
         if (input.value ==='') {
-          alert('Ingrese sus correo')  
-        }else{
-            alert('correo enviado con exito! ' +input.value);
-        }
-}
+           
+          Swal.fire({
 
+            title: 'Ingrese su correo ',
+         
+        });
+        }else{
+            Swal.fire({
+
+                title: 'Correo enviado con exito'+input.value
+            });
+        }
+       
+}
+ event.preventDefault()
   
     
     
