002f86b8811b89b4c43711eb8f747f04f374a7be
Denis Barreto <barretohanks@gmail.com>
2020-04-08 18:28:40 -0500

    todos los cambios

--- JavaScript/codigo.js
+++ JavaScript/codigo.js
@@ -2,14 +2,27 @@
         title:'Bienvenidos a mi sitio Web',
         text:'espero que te guste',
         icon: 'info'
-    });*/
+    
    
  
-   document.getElementById('btn').addEventListener('submit', function (e) {
-  alert('Hola');
- e.preventDefault();
-});
+   document.getElementById('btn').addEventListener('click', function (e) {
+    var input = document.getElementById('input').value;
+    if (input.value ==='') {
+       
+      Swal.fire({
+
+        title: 'Ingrese su correo ',
      
+    });
+    }else{
+        Swal.fire({
+
+            title: 'Correo enviado con exito'+input.value
+        });
+    }
+e.preventDefault()  
+});
     
+ });*/   
 
     
